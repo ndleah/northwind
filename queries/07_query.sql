31402372df57c77b5774532107d823481a7245f1
Leah Nguyen <nduongthucanh@gmail.com>
2021-09-18 18:18:22 +0700

    Update 07_query.sql

--- queries/07_query.sql
+++ queries/07_query.sql
@@ -20,7 +20,11 @@ Finally order the results by supplier region then category name then price range
 --Solution query
 SELECT
 	c.category_name,
-	s.region AS supplier_region,
+	CASE
+		WHEN s.country IN ('Australia', 'Singapore', 'Japan' ) THEN 'Asia-Pacific'
+		WHEN s.country IN ('US', 'Brazil', 'Canada') THEN 'America'
+		ELSE 'Europe'
+	END AS supplier_region,
 	p.unit_in_stock AS units_in_stock,
 	p.unit_on_order AS units_on_order,
 	p.reorder_level 
@@ -31,38 +35,38 @@ INNER JOIN categories AS c
 ON p.category_id = c.category_id
 WHERE s.region IS NOT NULL
 ORDER BY 
-	s.region,
+	supplier_region,
 	c.category_name,
 	p.unit_price;
-
---Result
+	
+--Result:
 +──────────────────+────────────────────+───────────────────+───────────────────+──────────────────+
 | "category_name"  | "supplier_region"  | "units_in_stock"  | "units_on_order"  | "reorder_level"  |
 +──────────────────+────────────────────+───────────────────+───────────────────+──────────────────+
-| "Dairy Products" | "Asturias"         | 22                | 30                | 30               |
-| "Dairy Products" | "Asturias"         | 86                | 0                 | 0                |
-| "Condiments"     | "LA"               | 4                 | 100               | 20               |
-| "Condiments"     | "LA"               | 76                | 0                 | 0                |
-| "Condiments"     | "LA"               | 0                 | 0                 | 0                |
-| "Condiments"     | "LA"               | 53                | 0                 | 0                |
-| "Seafood"        | "MA"               | 85                | 0                 | 10               |
-| "Seafood"        | "MA"               | 123               | 0                 | 30               |
-| "Condiments"     | "MI"               | 120               | 0                 | 25               |
-| "Condiments"     | "MI"               | 6                 | 0                 | 0                |
-| "Produce"        | "MI"               | 15                | 0                 | 10               |
-| "Grains/Cereals" | "NSW"              | 38                | 0                 | 25               |
-| "Meat/Poultry"   | "NSW"              | 0                 | 0                 | 0                |
-| "Produce"        | "NSW"              | 20                | 0                 | 10               |
-| "Beverages"      | "OR"               | 111               | 0                 | 15               |
-| "Beverages"      | "OR"               | 52                | 0                 | 10               |
-| "Beverages"      | "OR"               | 20                | 0                 | 15               |
-| "Condiments"     | "Québec"           | 113               | 0                 | 25               |
-| "Confections"    | "Québec"           | 17                | 0                 | 0                |
-| "Meat/Poultry"   | "Québec"           | 21                | 0                 | 10               |
-| "Meat/Poultry"   | "Québec"           | 115               | 0                 | 20               |
-| "Beverages"      | "Victoria"         | 15                | 10                | 30               |
-| "Condiments"     | "Victoria"         | 24                | 0                 | 5                |
-| "Confections"    | "Victoria"         | 29                | 0                 | 10               |
-| "Meat/Poultry"   | "Victoria"         | 0                 | 0                 | 0                |
-| "Seafood"        | "Victoria"         | 42                | 0                 | 0                |
+| "Condiments"     | "America"          | 113               | 0                 | 25               |
+| "Confections"    | "America"          | 17                | 0                 | 0                |
+| "Meat/Poultry"   | "America"          | 21                | 0                 | 10               |
+| "Meat/Poultry"   | "America"          | 115               | 0                 | 20               |
+| "Beverages"      | "Asia-Pacific"     | 15                | 10                | 30               |
+| "Condiments"     | "Asia-Pacific"     | 24                | 0                 | 5                |
+| "Confections"    | "Asia-Pacific"     | 29                | 0                 | 10               |
+| "Grains/Cereals" | "Asia-Pacific"     | 38                | 0                 | 25               |
+| "Meat/Poultry"   | "Asia-Pacific"     | 0                 | 0                 | 0                |
+| "Meat/Poultry"   | "Asia-Pacific"     | 0                 | 0                 | 0                |
+| "Produce"        | "Asia-Pacific"     | 20                | 0                 | 10               |
+| "Seafood"        | "Asia-Pacific"     | 42                | 0                 | 0                |
+| "Beverages"      | "Europe"           | 52                | 0                 | 10               |
+| "Beverages"      | "Europe"           | 111               | 0                 | 15               |
+| "Beverages"      | "Europe"           | 20                | 0                 | 15               |
+| "Condiments"     | "Europe"           | 4                 | 100               | 20               |
+| "Condiments"     | "Europe"           | 76                | 0                 | 0                |
+| "Condiments"     | "Europe"           | 0                 | 0                 | 0                |
+| "Condiments"     | "Europe"           | 53                | 0                 | 0                |
+| "Condiments"     | "Europe"           | 120               | 0                 | 25               |
+| "Condiments"     | "Europe"           | 6                 | 0                 | 0                |
+| "Dairy Products" | "Europe"           | 22                | 30                | 30               |
+| "Dairy Products" | "Europe"           | 86                | 0                 | 0                |
+| "Produce"        | "Europe"           | 15                | 0                 | 10               |
+| "Seafood"        | "Europe"           | 85                | 0                 | 10               |
+| "Seafood"        | "Europe"           | 123               | 0                 | 30               |
 +──────────────────+────────────────────+───────────────────+───────────────────+──────────────────+
