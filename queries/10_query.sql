aa6abd374b3769439599d5e28bf5dcb9f30c4ddd
Leah Nguyen <nduongthucanh@gmail.com>
2021-10-08 02:02:56 +0700

    update

--- queries/10_query.sql
+++ queries/10_query.sql
@@ -15,10 +15,38 @@ Finally order the results by category name (ascending) then total sales amount (
 WITH cte_kpi AS (
 SELECT
     c.category_name,
-    
     CONCAT(e.first_name, ' ', e.last_name) AS employee_full_name,
     ROUND(
 		  SUM(d.quantity * d.unit_price)::NUMERIC,
 		  2) AS total_sale_amount_including_discount
-    their percentage of total sales amount against the total sales amount across all employees (formatted to have only 2 decimals)
-5. their percentage of total sales amount against the total sales amount for each employees (formatted to have only 2 decimals)
+FROM employees AS e
+INNER JOIN orders AS o
+	ON e.employee_id = o.employee_id 
+INNER JOIN order_details AS d
+	ON o.order_id = d.order_id
+INNER JOIN products AS p
+	ON d.product_id = p.product_id
+INNER JOIN categories AS c
+	ON c.category_id = p.category_id
+GROUP BY 
+	category_name,
+	employee_full_name
+)
+SELECT 
+	*,
+	ROUND(
+		SUM(total_sale_amount_including_discount) / SUM(SUM(total_sale_amount_including_discount))
+			OVER (PARTITION BY employee_full_name) * 100,
+		2) AS percentage_of_employee_sales,
+	ROUND(	
+		SUM(total_sale_amount_including_discount) / SUM(SUM(total_sale_amount_including_discount))
+			OVER (PARTITION BY category_name) * 100,
+		2) AS percentage_of_category_sales
+FROM cte_kpi
+GROUP BY
+	category_name,
+	employee_full_name,
+	total_sale_amount_including_discount
+ORDER BY
+	category_name,
+	total_sale_amount_including_discount DESC;
---
+++ visualization.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(here)
+library(ggplot2)
+
+q1 <- read_csv(here("csv","01_results.csv"))
+q2 <- read_csv(here("csv","02_results.csv"))
+q3 <- read_csv(here("csv","03_results.csv"))
+q4 <- read_csv(here("csv","04_results.csv"))
+q5 <- read_csv(here("csv","05_results.csv"))
+q6 <- read_csv(here("csv","06_results.csv"))
+q7 <- read_csv(here("csv","07_results.csv"))
+q8 <- read_csv(here("csv","08_results.csv"))
+q9 <- read_csv(here("csv","09_results.csv"))
+q10 <- read_csv(here("csv","10_results.csv"))
+
+# Pricing Strategy
+
+#The Pricing Strategy
+#Question 1
+#Question 5
+#Question 6
+#Question 8
+
+q1
+q5
+q6
+q8
+
+#Logistics Performance
+#Question 2
+#Question 4
+#Question 7
+
+q2
+q4
+q7
+
+#Sale Performances
+#Question 3
+#Question 9
+#Question 10
+
+q3
+q9
+q10
